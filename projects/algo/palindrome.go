4b15e72802451f6a7eab3e8c53ede565f8f4d89e
Carlo Isidro B. Ditalo <dmgs.nerts@gmail.com>
2026-07-14 16:04:28 +0800

    algo: gofmt palindrome.go (comment alignment)

--- projects/algo/palindrome.go
+++ projects/algo/palindrome.go
@@ -18,8 +18,8 @@ func LongestPalindromicSubstring(s string) string {
 		return l + 1, r - 1
 	}
 	for i := 0; i < len(s); i++ {
-		l1, r1 := expand(i, i)     // odd-length, center at i
-		l2, r2 := expand(i, i+1)   // even-length, center between i and i+1
+		l1, r1 := expand(i, i)   // odd-length, center at i
+		l2, r2 := expand(i, i+1) // even-length, center between i and i+1
 		if r1-l1 > end-start {
 			start, end = l1, r1
 		}
